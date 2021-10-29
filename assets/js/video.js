544d0567810661e799b24c6cdb906b3496383fb7
WilliamTouboul <will.touboul@gmail.com>
2021-10-29 22:24:07 +0200

    redirection, menage, tout ca

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,26 +1,26 @@
-window.addEventListener("load", function () {
-  document.getElementById("iframe").pause();
-});
+// window.addEventListener("load", function () {
+//   document.getElementById("iframe").pause();
+// });
 
-document.querySelector('').addEventListener('click', function () {
-  event.preventDefault();
-  var iframe = document.querySelector('#iframe');
-  document.getElementById("header").style.display = 'none';
-  iframe.style.display = "block";
-  iframe.play();
-  setTimeout(function () {
-    window.location.href = "game.html";
-  }, 5500);
-});
+// document.querySelector('').addEventListener('click', function () {
+//   event.preventDefault();
+//   var iframe = document.querySelector('#iframe');
+//   document.getElementById("header").style.display = 'none';
+//   iframe.style.display = "block";
+//   iframe.play();
+//   setTimeout(function () {
+//     window.location.href = "game.html";
+//   }, 5500);
+// });
 
 
-document.querySelector('').addEventListener('click', function () {
-  event.preventDefault();
-  var iframe = document.querySelector('#iframe');
-  document.getElementById("header").style.display = 'none';
-  iframe.style.display = "block";
-  iframe.play();
-  setTimeout(function () {
-    window.location.href = "game.html";
-  }, 5500);
-});
+// document.querySelector('').addEventListener('click', function () {
+//   event.preventDefault();
+//   var iframe = document.querySelector('#iframe');
+//   document.getElementById("header").style.display = 'none';
+//   iframe.style.display = "block";
+//   iframe.play();
+//   setTimeout(function () {
+//     window.location.href = "game.html";
+//   }, 5500);
+// });
